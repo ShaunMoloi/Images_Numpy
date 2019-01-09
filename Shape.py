59716ec946bb96c643a559085efd5c46867557aa
Shaun Moloi <34385762+ShaunMoloi@users.noreply.github.com>
2019-01-09 12:57:57 +0200

    Added correct file with functionality

--- Shape.py
+++ Shape.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
 
-import cv2
-import numpy as np
 def draw_circle(event,x,y,flags,param):
     
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img,(x,y),100,(0,255,0),-1)
-
+    
+    if event == cv2.EVENT_RBUTTONDOWN:
+    
+        cv2.circle(img,(x,y),100,(0,0,255),-1)
+        
+    if event == cv2.EVENT_MBUTTONDOWN:
+        cv2.circle(img,(x,y),100,(255,255,255),-1) 
+        
 cv2.namedWindow(winname='drawing')
 
 cv2.setMouseCallback('drawing',draw_circle)
